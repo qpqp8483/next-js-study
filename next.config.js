846c77c2725f838b69b7120227755c532d3fa914
chanwoo <wkdcksdn1996@gmail.com>
2022-10-18 00:55:42 +0900

    next-js redirects, rewrites 공부

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+const { redirect } = require("next/dist/server/api-utils");
+
 /** @type {import('next').NextConfig} */
+const API_KEY = "08e32ba35715aadb4433f7e086b1fcaa";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
